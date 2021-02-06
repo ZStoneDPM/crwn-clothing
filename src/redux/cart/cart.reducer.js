b677f2db02b6b28c33cf0e5223e457eb805ede81
ZStoneDPM <zacharystone2028@gmail.com>
2021-02-06 09:26:13 -0600

    added adItem and subtractItem functionalitiy to checkout component arrows

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -17,6 +17,17 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload) //append items in cart <<<
+            }
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems
+                .filter(cartItem => cartItem.id !== action.payload.id)//return only if id's do not match-- delete
+            }
+        case CartActionTypes.SUBTRACT_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)// remove one if exists, function in utils
             }
         default:
             return state;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,22 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 };
+
+export const removeItemFromCart = (cartItems, cartItemsToRemove) => {
+    //find in cartItems object where cartitems to remove has same id
+    const exisitingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemsToRemove.id
+    )
+
+    if(exisitingCartItem.quantity === 1) {
+        //if ids do not match, return cartitems as is
+        return cartItems.filter(cartItem => cartItem.id !== cartItemsToRemove.id) 
+    }
+
+    return cartItems.map(cartItem =>
+        //if id matches, subtract one
+        cartItem.id === cartItemsToRemove.id 
+        ? {...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem
+    )
+}
